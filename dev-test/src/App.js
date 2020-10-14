1bb0d338f2c5eb9cd81c25d7397381c3644cf663
alejandro-g <diego.galdamez@unitec.edu>
2020-10-14 10:02:08 -0600

    agregando apollo client y sus respectivos imports para que react entienda y pueda hacer queries al endpoint deseado

--- dev-test/src/App.js
+++ dev-test/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost'; 
+import {ApolloProvider} from 'react-apollo'; 
 
 //components imported and nested
 import PostList from './components/PostList'; 
 
+//apollo client setup 
+//uri es el endpoint al cual se le estan haciendo requests/queries
+const client = new ApolloClient({
+  uri: 'https://graphqlzero.almansi.me/api',
+})
+
 function App() {
   return (
-    <div id="main">
-      <h1>List of posts</h1>
-      <PostList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>List of posts</h1>
+        <PostList/>
+      </div>
+    </ApolloProvider>
   );
 }
 
