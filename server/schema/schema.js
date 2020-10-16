40a097cafa43e54504479d2db952959a28149f69
alejandro-g <diego.galdamez@unitec.edu>
2020-10-16 09:22:57 -0600

    se creo dummy data para los users y ya logra hacer una relacion entre los posts y su respectivo user usando el id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,24 +10,44 @@ const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
 //dummy data
 
 var posts = [
-    {title: 'Hello', user: 'Erick', username:'chief', body: 'asjkajaa', id: '1'},
-    {title: 'Goodbye', user: 'Matt', username:'master', body: 'qwerty', id: '2'},
-    {title: 'Food', user: 'Stonie', username:'cart', body: 'aadasas', id: '3'}, 
+    {title: 'Hello', body: 'asjkajaa', userID: '2', id: '1', userID: '1'},
+    {title: 'Goodbye', body: 'qwerty', userID: '3', id: '2', userID: '2'},
+    {title: 'Food', body: 'aadasas', userID: '4', id: '3', userID: '3'}, 
+]
+
+var users = [
+    {name: "Leanne Graham", userName: "Bert", id: '1'}, 
+    {name: "Ervin Howell", userName: "Antonette", id: '2'},
+    {name: "Clementine Bauch", userName: "Samantha", id: '3'},
 ]
 
 //definiendo como se "verá" el objeto dentro del grafo
-//los fields se hacen wrap en una funcion para que si existen types 
-//variados, entonces se puede prevenir errores de referencia 
+//los fields se hacen wrap en una funcion para que si existen dependencias de untipo a otro
+//entonces no exista un error  
 const PostType = new GraphQLObjectType ({
     name: 'Post',
     fields: () => ({
         id: {type: GraphQLID},
         title: {type: GraphQLString},
-        user: {type: GraphQLString}, 
-        username: {type: GraphQLString}, 
-        body: {type: GraphQLString}
+        body: {type: GraphQLString},
+        user: {
+            type: UserType,
+            resolve(parent, args){
+                return _.find(users, {id: parent.userID});
+            }
+        }
     })
 }); 
+
+const UserType = new GraphQLObjectType ({
+    name: 'User',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        userName: {type: GraphQLString}
+    })
+}); 
+
 
 //el rootquery sirve para que el front end pueda ingresar al graph con el query que el usuario introduzca.
 //Pidiendo como argumentos el id del post que quiere encontrar
@@ -40,6 +60,13 @@ const RootQuery = new GraphQLObjectType ({
             resolve(parent, args){
                 //code to get data from db/other source
                 return _.find(posts, {id: args.id}); 
+            }
+        },
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLID}}, 
+            resolve(parent, args){
+                return _.find(users, {id: args.id}); 
             }
         }
     }
