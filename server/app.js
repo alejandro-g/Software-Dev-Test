5355338d48b957533cc9e1f53e677ab546bdc5ad
alejandro-g <diego.galdamez@unitec.edu>
2020-10-16 11:40:50 -0600

    estableciendo conexion con mongoDB

--- server/app.js
+++ server/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP; //permite que la aplicacion pueda interactuar y funcionar con graphql 
 const schema = require('./schema/schema'); 
 const cors = require('cors'); 
+const mongoose = require('mongoose'); 
 
 const app = express();  
 
 //permitir cross-origin requests
 app.use(cors()); 
+
+mongoose.connect('mongodb+srv://sledge:test123@test.14c2m.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology:true}); 
+mongoose.connection.once('open', () =>{
+    console.log("connected to db"); 
+});
 
 app.use('/graphql', graphqlHTTP({
     schema, 
