7f5ef30d0315603cd9aaa9aafa428c48901756ba
alejandro-g <diego.galdamez@unitec.edu>
2020-10-18 01:38:28 -0600

    Updates

---
+++ precache-manifest.070b9225e282fc8efe667b142996b19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df5071f26df62b0e9938f6adff905d8",
+    "url": "/Software-Dev-Test/index.html"
+  },
+  {
+    "revision": "c5ad5182843a2e3e341e",
+    "url": "/Software-Dev-Test/static/css/main.d5dc74ff.chunk.css"
+  },
+  {
+    "revision": "0391505ff29de67247e8",
+    "url": "/Software-Dev-Test/static/js/2.5f92e43e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Software-Dev-Test/static/js/2.5f92e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ad5182843a2e3e341e",
+    "url": "/Software-Dev-Test/static/js/main.cccd363b.chunk.js"
+  },
+  {
+    "revision": "9d2782a4689ed457b4ff",
+    "url": "/Software-Dev-Test/static/js/runtime-main.240ae98c.js"
+  }
+]);
